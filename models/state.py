b6f440603eff11d34c3bcc90884a82c3b225f874
MbaliFelecia <ramakgashambali@gmail.com>
2023-10-15 07:03:07 -0700

    state class

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Class State that inherits from BaseModel
+takes in the state name """
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Class State that inherits from BaseModel """
+
+    name = ""
